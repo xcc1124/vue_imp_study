f9caf7f78eb0db3f9fd822c0e939652402740187
xiaochuchun <2398506993@qq.com>
2021-06-29 11:20:41 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="home">
-    <el-button>父子组件通信</el-button>
+    <el-button>计算属性和侦听器</el-button>
+    <el-button>生命周期</el-button>
+
+    <el-divider></el-divider>
+    <el-button>数组对象的方法</el-button>
+    <el-button>字符串对象的方法</el-button>
+    <el-button>字典对象的方法</el-button>
+
+    <el-divider></el-divider>
+    <el-button>同步/异步</el-button>
+
+    <el-divider></el-divider>
+    <el-button>插槽</el-button>
+    <el-button>组件化</el-button>
+    <el-button>class/style绑定</el-button>
+    <el-button>必会API</el-button>
+    <el-button>动画库</el-button>
+    <el-button>模板与渲染函数</el-button>
+    <el-button>过滤器</el-button>
+
+    <el-divider></el-divider>
+    <el-button>数据mock</el-button>
+    <el-button>vuex</el-button>
+    <el-button>$bus</el-button>
+    <el-button>stronge</el-button>
+    <el-button>路由（动态/状态保存）</el-button>
+
+    <el-divider></el-divider>
+    <el-button>父子（兄弟）组件通信</el-button>
+
+    <el-divider></el-divider>
+    <el-button>工作中用过的方法</el-button>
   </div>
 </template>
 
